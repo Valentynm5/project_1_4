56b419829f814b7e53072283e784fb9373964734
Valentynm5 <valentynm5cs@gmail.com>
2023-04-03 10:11:30 +0300

    Init commit

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int V;
+
+    cout << "number" <<endl;
+    cout << "Enter number ";
+    cin >> V;
+    float A= V * V;
+    cout << "Here is " << A << endl;
+    return 0;
+}
